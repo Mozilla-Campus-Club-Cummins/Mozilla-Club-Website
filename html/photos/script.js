afaaf1927f0706d87d8b928c0bb38be5751362b0
Manasi Deshmukh <115893690+ManasiGDeshmukh@users.noreply.github.com>
2024-02-06 20:03:02 +0530

    Add files via upload

---
+++ html/photos/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const navbarList = document.querySelector('.navbar-list');
+  
+    menuIcon.addEventListener('click', function () {
+      navbarList.classList.toggle('show');
+    });
+  
+    window.addEventListener('resize', function () {
+      if (window.innerWidth > 768) {
+        navbarList.classList.remove('show');
+      }
+    });
+  });
+  
